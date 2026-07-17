093a4fe27ef12ace6958de1b09af598f29ddbcef
Ashmit Gupta <ashmitgupta637@gmail.com>
2026-07-17 19:48:53 +0530

    Time: 0 ms (100%), Space: 22.1 MB (33.04%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxdepth(TreeNode* root) {
+        if(root == nullptr) return 0  ;
+        int lh = maxdepth(root->left)  ;
+        int rh = maxdepth(root->right) ; 
+
+        return 1+max(lh , rh) ; 
+    }
+    TreeNode* helper(TreeNode* root , int curr , int maxd) {
+        if(root == nullptr) {
+            return nullptr ; 
+        }
+        if(curr == maxd) return root ; 
+
+        TreeNode* lh = helper(root->left , curr+1 ,maxd) ; 
+        TreeNode* rh =  helper(root->right , curr+1 , maxd) ; 
+
+        if(lh != nullptr && rh!= nullptr) {
+            return root ; 
+        }
+
+        return lh != nullptr ? lh : rh ; 
+    }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        int maxd = maxdepth(root) ; 
+        return helper(root , 1 , maxd )  ;
+    }
+};
